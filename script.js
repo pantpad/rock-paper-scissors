545195a336d13a482ac4bae84baa302cb5505577
pantpad <138708449+pantpad@users.noreply.github.com>
2023-07-24 17:37:18 +0200

    Make the game logic work in the UI

--- script.js
+++ script.js
@@ -104,7 +104,6 @@ function playRPSRound(){
     return `Vincitore: ${roundResult(cpu,user)}\nCPU: ${cpuPoints}\nUSER: ${userPoints}`;
     
 }
-
 /**
  * 
  */
@@ -156,12 +155,25 @@ function roundResult(computerChoice,userChoice,){
 const startPlayingBtn = document.getElementById('play');
 const userChoices = document.querySelectorAll('.user');
 
+const cpuChoice = document.getElementById('cpu-choice');
+const userChoice = document.getElementById('user-choice');
+
+const cpuResult = document.querySelector('.cpu-result');
+const userResult = document.querySelector('.user-result');
+
+const cpuScore = document.getElementById('cpu-score');
+const userScore = document.getElementById('user-score');
+
 userChoices.forEach(choice => choice.addEventListener('click',(e) => {
     
-    playRPSUIRound(e.target.value);
+    playRPSUIRound(e);
+    if(isGameOver(cpuPoints,userPoints)){
+        (cpuPoints>userPoints ? setTextBtn(startPlayingBtn,"HAI PERSO!") : setTextBtn(startPlayingBtn,"HAI VINTO!"));
+        startEndRound();
+    }    
     //playRound(), prende input cpu, prende input user, compara, segna a schermo il vincitore o il pareggio, controlla se il game e' finito, 
     //mette a schermo bottone di play game o play round in base all'andamento del gioco
-
+    
 }));
 
 /**
@@ -196,20 +208,106 @@ function setTextBtn(button,text){
 
 
 startPlayingBtn.addEventListener('click',(e) => {
+    if(startPlayingBtn.value!="Play Game" && startPlayingBtn.value!="Play Round"){
+        resetPoints();
+        resetResults();
+        setTextBtn(startPlayingBtn,"Play Game");
+    }
     startEndRound();
     setTextBtn(startPlayingBtn,"Play Round");
 });
 
 /**
  * playRPSUIRound() plays a single rock paper scissors round
- * 
+ * playRound(), prende input cpu, prende input user, compara, segna a schermo il vincitore o il pareggio, aggiorna i punti, controlla se il game e' finito, 
+   mette a schermo bottone di play game o play round in base all'andamento del gioco
  * **/
-function playRPSUIRound(userChoice){
+function playRPSUIRound(user){
+    //reset previous div text results
+    resetResults();
     let cpu = getComputerChoice();
 
-    //playRound(), prende input cpu, prende input user, compara, segna a schermo il vincitore o il pareggio, controlla se il game e' finito, 
-    //mette a schermo bottone di play game o play round in base all'andamento del gioco
-    console.log(cpu);
-    console.log(userChoice);
+    //put on screen -> cpu and user choice
+    cpuChoice.textContent = (cpu == 1 ? "ROCK" : (cpu == 2 ? "PAPER" : "SCISSORS"));
+    userChoice.textContent = user.target.textContent.toUpperCase();
+    //compare them to add points and return who WON
+    switch(roundResultUI(cpu,user.target.value)){
+        case "CPU":
+            cpuResult.textContent = "WINNER";
+            userResult.textContent = "LOSER";
+            break;
+        case "USER":
+            userResult.textContent = "WINNER";
+            cpuResult.textContent = "LOSER";
+            break;
+        case "DRAW":
+            cpuResult.textContent = userResult.textContent = "DRAW";
+            break;
+    }
+
+    //add point to screen
+    cpuScore.textContent = cpuPoints;
+    userScore.textContent = userPoints;
+
+    //check if game is over
     
+    
+}
+
+function resetChoices(){
+    cpuChoice.textContent = "";
+    userChoice.textContent = "";
+}
+
+function resetResults(){
+    userResult.textContent = cpuResult.textContent =  "";
+}
+
+function resetPoints(){
+    userPoints = 0;
+    cpuPoints = 0;
+    userScore.textContent = "USER: 0";
+    cpuScore.textContent ="CPU: 0";
+}
+
+/**
+ * compara, segna a schermo il vincitore o il pareggio, aggiorna i punti
+ * 
+ */
+function roundResultUI(computerChoice,userChoice,){
+    if(computerChoice == userChoice){
+        return "DRAW";
+    }
+
+    if(computerChoice == ROCK){
+        if(userChoice == SCISSORS){
+            cpuPoints++;
+            return "CPU";
+        }
+        userPoints++;
+        return "USER";
+    }
+
+    if(computerChoice == PAPER){
+        if(userChoice == ROCK){
+            cpuPoints++;
+            return "CPU";
+        }
+        userPoints++;
+        return "USER";
+    }
+
+    if(computerChoice == SCISSORS){
+        if(userChoice == PAPER){        
+            cpuPoints++;
+            return "CPU";
+        }
+        userPoints++;
+        return "USER";
+    }
+
+}
+
+function isGameOver(cpuPoints,userPoints){
+    return (cpuPoints >= 5 || userPoints >= 5);
 }
